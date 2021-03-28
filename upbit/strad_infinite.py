2aa1281b213a3eed2a543ab62f67f9b6cbee4fda
gnlenfn <akenfn513@gmail.com>
2021-03-29 00:38:07 +0900

    add nubmering with class

--- upbit/strad_infinite.py
+++ upbit/strad_infinite.py
@@ -1,6 +1,5 @@
 import telegram_bot
 import upbit_basic
-import datetime
 import time
 import logging
 
@@ -31,104 +30,133 @@ stream_formatter = logging.Formatter(f'[%(asctime)s]:%(levelname)s\n{a}\n%(messa
 stream_handler.setFormatter(stream_formatter)
 logger.addHandler(stream_handler)
 
+class infinite:
+    def __init__(self):
+        self.num = 0
 
-def infinite_bid(target, profit, min_order):
-    try:
-        now = datetime.datetime.now()
-        minimum_order = min_order
-        non_budget = float(upbit_basic.get_coin_account("KRW")['balance']) - 4_000_000.0
-        minute_close_price = upbit_basic.get_trade_price("KRW-"+target, "minutes", "1", "1")[0]['trade_price']
-        order_vol = minimum_order / minute_close_price
+    def infinite_bid(self, target, min_order):
+        try:
+            minimum_order = min_order
+            #non_budget = float(upbit_basic.get_coin_account("KRW")['balance']) - 4_000_000.0
+            minute_close_price = upbit_basic.get_trade_price("KRW-"+target, "minutes", "1", "1")[0]['trade_price']
+            order_vol = minimum_order / minute_close_price
 
-        if not upbit_basic.get_coin_account(target): # target coin 보유 없으면
-            upbit_basic.order("KRW-"+target, 'bid', order_vol, 'limit', minute_close_price)
-
-            time.sleep(30)
-            avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
-            logger.info(
-                f"첫 매수 시작1\n"+
-                f"{now.strftime('%Y-%m-%d %H:%M:%S')}\n"+
-                f"매수 수량: {target} {order_vol:.8f} 개\n"+
-                f"매수 가격: {minute_close_price}\n"+
-                f"매수 평단: {avg_buy_after}\n"+
-                f"현금 잔고: {upbit_basic.get_coin_account('KRW')['balance']} 원")
-        else:
-            current_avg_price = float(upbit_basic.get_coin_account(target)['avg_buy_price'])
-            current_volume = float(upbit_basic.get_coin_account(target)['balance'])
-            cash_left = float(upbit_basic.get_coin_account("KRW")['balance']) - non_budget
-            
-            if cash_left < minimum_order:  # 잔고 없으면 (손절 or 목표도달 못한 익절)
-                logger.info(
-                    f"전체매도\n"+
-                    f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']:.2f}\n"+
-                    f"매도 수량: {target} {current_volume:.8f} 개\n"+
-                    f"매도 가격: {current_avg_price}\n"+
-                    f"실현 손익: {current_volume * minute_close_price} 원\n"+
-                    f"현금 잔고: {upbit_basic.get_coin_account('KRW')['balance']:.2f} 원")
-                upbit_basic.order(market="KRW-"+target, side='ask', vol=current_volume,
-                    price=minute_close_price, types='limit')
-                
-                time.sleep(30)
-                upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol,
-                    price=minute_close_price, types='limit')
+            if not upbit_basic.get_coin_account(target): # target coin 보유 없으면
+                upbit_basic.order("KRW-"+target, 'bid', order_vol, 'limit', minute_close_price)
+                self.num += 1
 
                 time.sleep(30)
                 avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
                 logger.info(
-                    f"매도 후 1회차 매수 시작\n"+
-                    f"매수 수량: {order_vol}\n"+
+                    # 첫 매수
+                    f"{self.num}차 매수 시작1\n"+
+                    f"매수 수량: {target} {order_vol:,.4f} 개\n"+
                     f"매수 가격: {minute_close_price}\n"+
-                    f"현재 수량: {target} {current_volume:.8f} 개\n"+
-                    #f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']}\n"+
-                    f"현재 평단: {avg_buy_after}\n"+
-                    f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
-
-
-            elif current_avg_price > minute_close_price:  # 평단보다 현재가격이 낮은 가격이면 매수
-                upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol, 
-                    price=minute_close_price, types='limit')
-
-                time.sleep(30)
-                avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
-                logger.info(
-                    f"추가 매수\n"+
-                    f"매수 수량: {order_vol}\n"+
-                    f"매수 가격: {minute_close_price}\n"+
-                    f"현재 수량: {target} {current_volume:.8f} 개\n"+
-                    f"현재 평단: {avg_buy_after}\n"+
-                    f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
-
-            # elif current_avg_price * (1.0 + profit) <= float(minute_close_price):  # 평단 * 1.1 보다 현재 가격이 높으면 매도
-            #     # print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} Sold all {target} with benefit")
-            #     logger.info(
-            #         f"상승으로 익절\n"+
-            #         f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']:}\n"+
-            #         f"매도 수량: {target} {current_volume:.8f} 개\n"+
-            #         f"매도 가격: {current_avg_price}\n"+
-            #         f"실현 수익: {current_volume * minute_close_price} 원\n"+
-            #         f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
-            #     upbit_basic.order(market="KRW-"+target, side='ask', vol=current_volume,
-            #         price=minute_close_price, types='limit')  # 익절 작업
-
-            #     time.sleep(5)
-            #     upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol, #'0.01269036',
-            #         price=minute_close_price, types='limit')   # 익절 후 재매수
-                
+                    f"매수 평단: {avg_buy_after}\n"+
+                    f"현금 잔고: {upbit_basic.get_coin_account('KRW')['balance']:,.2f} 원")
             else:
-                upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol, 
-                    price=minute_close_price, types='limit')
+                current_avg_price = float(upbit_basic.get_coin_account(target)['avg_buy_price'])
+                current_volume = float(upbit_basic.get_coin_account(target)['balance'])
+                cash_left = float(upbit_basic.get_coin_account("KRW")['balance'])# - non_budget
+                
+                if cash_left < minimum_order:  # 잔고 없으면 (손절 or 목표도달 못한 익절)
+                    logger.info(
+                        f"전체매도\n"+
+                        f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']:.2f}\n"+
+                        f"매도 수량: {target} {current_volume:,.4f} 개\n"+
+                        f"매도 가격: {current_avg_price}\n"+
+                        f"실현 손익: {current_volume * minute_close_price:,.2f} 원\n"+
+                        f"현금 잔고: {upbit_basic.get_coin_account('KRW')['balance']:,.2f} 원")
+                    upbit_basic.order(market="KRW-"+target, side='ask', vol=current_volume,
+                        price=minute_close_price, types='limit')
+                    self.num = 0
+
+                    time.sleep(30)
+                    upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol,
+                        price=minute_close_price, types='limit')
+                    self.num += 1
+                    time.sleep(30)
+                    avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
+                    logger.info(
+                        # 첫 매수
+                        f"{self.num}회차 매수\n"+
+                        f"매수 수량: {order_vol:,.4f}\n"+
+                        f"매수 가격: {minute_close_price}\n"+
+                        f"현재 수량: {target} {current_volume:.4f} 개\n"+
+                        #f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']}\n"+
+                        f"현재 평단: {avg_buy_after}\n"+
+                        f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
+
+
+                elif current_avg_price > minute_close_price:  # 평단보다 현재가격이 낮은 가격이면 매수
+                    upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol, 
+                        price=minute_close_price, types='limit')
+                    self.num +=1
+
+                    time.sleep(30)
+                    avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
+                    logger.info(
+                        # 추매 
+                        f"{self.num}회차 매수\n"+
+                        f"매수 수량: {order_vol:,.4f}\n"+
+                        f"매수 가격: {minute_close_price}\n"+
+                        f"현재 수량: {target} {current_volume:.4f} 개\n"+
+                        f"현재 평단: {avg_buy_after}\n"+
+                        f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
+                    
+                else:
+                    upbit_basic.order(market="KRW-"+target, side='bid', vol=order_vol, 
+                        price=minute_close_price, types='limit')
+                    self.num += 1
+
+                    time.sleep(30)
+                    avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
+                    logger.info(
+                        # 추매
+                        f"{self.num}회차 매수\n"+
+                        f"매수 수량: {order_vol:,.4f}\n"+
+                        f"매수 가격: {minute_close_price}\n"+
+                        f"현재 수량: {target} {current_volume:.4f} 개\n"+
+                        f"현재 평단: {avg_buy_after}\n"+
+                        f"현금 잔고: {float(upbit_basic.get_coin_account('KRW')['balance']):,.2f} 원")
+        
+        except Exception as ex:
+            logger.error("error on infinite_bid")
+            logger.exception(ex)
+
+        
+    def sell_make_profit(self, target, profit, min_order):
+        try:
+            minimum_order = min_order
+            minute_close_price = upbit_basic.get_trade_price("KRW-"+target, "minutes", "1", "1")[0]['trade_price']
+            order_vol = minimum_order / minute_close_price
+            if not upbit_basic.get_coin_account(target): # target coin 보유 없으면
+                upbit_basic.order("KRW-"+target, 'bid', order_vol, 'limit', minute_close_price)
+                self.num += 1
 
                 time.sleep(30)
-                avg_buy_after = upbit_basic.get_coin_account(target)['avg_buy_price']
                 logger.info(
-                    f"추가 매수\n"+
-                    f"매수 수량: {order_vol}\n"+
-                    f"매수 가격: {minute_close_price}\n"+
-                    f"현재 수량: {target} {current_volume:.8f} 개\n"+
-                    f"현재 평단: {avg_buy_after}\n"+
-                    f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
-    
-    except Exception as ex:
-        logger.error("error on infinite_bid")
-        logger.exception(ex)
+                    # 첫 매수
+                    f"{self.num}회차 매수!\n"+
+                    f"매수 수량: {target} {order_vol:.4f} 개\n"+
+                    f"매수 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']}\n"+
+                    f"현금 잔고: {float(upbit_basic.get_coin_account('KRW')['balance']):,.2f} 원")
+            else:
+                current_avg_price = float(upbit_basic.get_coin_account(target)['avg_buy_price'])
+                current_volume = float(upbit_basic.get_coin_account(target)['balance'])
+                if current_avg_price * (1.0 + profit) <= float(minute_close_price):  # 평단 * profit 보다 현재 가격이 높으면 매도
+                    logger.info(
+                        f"상승으로 익절\n"+
+                        f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']:}\n"+
+                        f"매도 수량: {target} {current_volume:.4f} 개\n"+
+                        f"매도 가격: {minute_close_price}\n"+
+                        f"실현 손익: {current_volume * minute_close_price:,.2f} 원\n"+
+                        f"현금 잔고: {float(upbit_basic.get_coin_account('KRW')['balance']):,.2f} 원")
+                    upbit_basic.order(market="KRW-"+target, side='ask', vol=current_volume,
+                        price=minute_close_price, types='limit')  # 익절 작업
+                    self.num = 0
+
+        except Exception as ex:
+            logger.error("error on sell_make_profit")
+            logger.exception(ex)
 
--- upbit/upbit_api_trading.py
+++ upbit/upbit_api_trading.py
@@ -7,7 +7,8 @@ import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 
-import strad_infinite
+#import strad_infinite
+from strad_infinite import infinite
 import telegram_bot
 import upbit_basic
 
@@ -33,58 +34,27 @@ stream_formatter = logging.Formatter(f'[%(asctime)s]:%(levelname)s\n{a}\n%(messa
 stream_handler.setFormatter(stream_formatter)
 logger.addHandler(stream_handler)
 
-# 비트 가격알림
-def BTCprice_alarm():
-    data = upbit_basic.get_trade_price("KRW-BTC")[0]
-    open_p, low_p, high_p = data['opening_price'], data['low_price'], data['high_price']
-    if open_p * 0.99 >= low_p:
-        logger.info(f"🔻🔻🔻 BTC 폭락!! 👎🔻🔻🔻\n\
-        현재가격: {data['trade_price']}")
-        # print("!! BTC alarm !!")
+# 가격알림
+class alarm:
+    # 비트가격 알람
+    def BTCprice_alarm(self):
+        data = upbit_basic.get_trade_price("KRW-BTC")[0]
+        open_p, low_p, high_p = data['opening_price'], data['low_price'], data['high_price']
+        if open_p * 0.99 >= low_p:
+            logger.info(f"🔽🔽🔽 BTC 폭락!! 👎🔽🔽🔽\n\
+            현재가격: {data['trade_price']}")
 
-    elif open_p * 1.01 <= high_p:
-        logger.info(f"🔺🔺🔺 BTC 폭등각? 👍🔺🔺🔺\n\
-        현재가격: {data['trade_price']}")
-        # print("!! BTC alarm !!")
+        elif open_p * 1.01 <= high_p:
+            logger.info(f"🔺🔺🔺 BTC 폭등각? 👍🔺🔺🔺\n\
+            현재가격: {data['trade_price']}")
 
-def target_price(target):
-    # print(f"Seaching {target} price...")
-    data = upbit_basic.get_trade_price("KRW-"+target)[0]
-    logger.info(f"📈{target} 가격 알리미\n"+
-                            f"{data['trade_price']} 원\n"
-                            )
+    def target_price(self, target):
+        data = upbit_basic.get_trade_price("KRW-"+target)[0]
+        logger.info(f"📈{target} 가격 알리미\n"+
+                                f"{data['trade_price']} 원\n"
+                                )
 ####################################################################
-    
-def sell_make_profit(target, profit, min_order):
-    try:
-        minimum_order = min_order
-        minute_close_price = upbit_basic.get_trade_price("KRW-"+target, "minutes", "1", "1")[0]['trade_price']
-        order_vol = minimum_order / minute_close_price
-        if not upbit_basic.get_coin_account(target): # target coin 보유 없으면
-            upbit_basic.order("KRW-"+target, 'bid', order_vol, 'limit', minute_close_price)
 
-            time.sleep(30)
-            logger.info(
-                f"첫 매수 시작2\n"+
-                f"매수 수량: {target} {order_vol:.8f} 개\n"+
-                f"매수 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']}\n"+
-                f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
-        else:
-            current_avg_price = float(upbit_basic.get_coin_account(target)['avg_buy_price'])
-            current_volume = float(upbit_basic.get_coin_account(target)['balance'])
-            if current_avg_price * (1.0 + profit) <= float(minute_close_price):  # 평단 * profit 보다 현재 가격이 높으면 매도
-                logger.info(
-                    f"상승으로 익절\n"+
-                    f"현재 평단: {upbit_basic.get_coin_account(target)['avg_buy_price']:}\n"+
-                    f"매도 수량: {target} {current_volume:.8f} 개\n"+
-                    f"매도 가격: {current_avg_price}\n"+
-                    f"실현 손익: {current_volume * minute_close_price} 원\n"+
-                    f"현금 잔고: {round(float(upbit_basic.get_coin_account('KRW')['balance']), 3)} 원")
-                upbit_basic.order(market="KRW-"+target, side='ask', vol=current_volume,
-                    price=minute_close_price, types='limit')  # 익절 작업
-    except Exception as ex:
-        logger.error("error on sell_make_profit")
-        logger.exception(ex)
 
 
 def main():
@@ -96,13 +66,17 @@ def main():
 
     buy_time = '4, 12, 20'
 
+    my_strategy = infinite()
+    my_alarm = alarm()
     ############### schedules ###############
     sched = BackgroundScheduler()
-    sched.add_job(lambda: strad_infinite.infinite_bid(args.target_coin, args.profit, args.min), 
+    # strategies
+    sched.add_job(lambda: my_strategy.infinite_bid(args.target_coin, args.min), 
                 'cron', hour=buy_time, id="buy_1")
-    sched.add_job(BTCprice_alarm, 'interval', minutes=1)
-    sched.add_job(lambda: target_price(args.target_coin), 'cron', hour='1, 9, 13, 17, 21')
-    sched.add_job(lambda: sell_make_profit(args.target_coin, args.profit, args.min), 'interval', seconds=10)
+    sched.add_job(lambda: my_strategy.sell_make_profit(args.target_coin, args.profit, args.min), 'interval', seconds=10)
+    # alarms
+    sched.add_job(my_alarm.BTCprice_alarm, 'interval', minutes=1)
+    sched.add_job(lambda: my_alarm.target_price(args.target_coin), 'cron', hour='1, 9, 13, 17, 21')
     ##########################################
     logger.info(f"{args.target_coin} 한무 매수 시작\n" +
                             f"매수 예정 시간 {buy_time}시")
